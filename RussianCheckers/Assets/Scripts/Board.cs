205dde9c9aa6f0cbd5758128ad710b91343c6111
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-04-06 17:02:52 +0300

    RemakeCameraWinAndDraw

--- RussianCheckers/Assets/Scripts/Board.cs
+++ RussianCheckers/Assets/Scripts/Board.cs
@@ -13,7 +13,8 @@ public class Board : MonoBehaviour
     List<Checker> beatCheckers = new List<Checker>();
     private Checker selectedChecker;
     private bool isWhiteTurn;
-
+    private bool wasBeat;
+    private int onlyKingTurns;
     void Start()
     {
         transform.position = new Vector3(3.5f, -0.14f, 3.5f);
@@ -35,9 +36,7 @@ public class Board : MonoBehaviour
                 if (checker != null && checker.Data.isWhite == isWhiteTurn && beatCheckers.Count == 0) 
                     selectedChecker = checker;
                 else if (checker != null && checker.Data.isWhite == isWhiteTurn && beatCheckers.Contains(checker))
-                {
                     selectedChecker = checker;
-                }
             }
             if (selectedChecker != null)
             {
@@ -51,7 +50,8 @@ public class Board : MonoBehaviour
                         selectedChecker.transform.position = new Vector3(originPosition.x, 0, originPosition.y);
                         selectedChecker = null;
                         return;
-                    }if (originPosition == mouseDownPosition || !IsCorrectMove(mouseDownPosition))
+                    }
+                    if (originPosition == mouseDownPosition || !IsCorrectMove(mouseDownPosition))
                     {
                         selectedChecker.transform.position = new Vector3(originPosition.x, 0, originPosition.y);
                         selectedChecker = null;
@@ -60,17 +60,19 @@ public class Board : MonoBehaviour
                     selectedChecker.Data.position = mouseDownPosition;
                     selectedChecker.transform.position = new Vector3(mouseDownPosition.x, 0, mouseDownPosition.y);
                     if ((selectedChecker.transform.position.z == 7 && selectedChecker.Data.isWhite) || (selectedChecker.transform.position.z == 0 && !selectedChecker.Data.isWhite))
-                    {
                         selectedChecker.MakeKing();
+                    if (wasBeat)
+                    {
+                        beatCheckers = new List<Checker>();
+                        beatCheckers.AddRange(BeatMoves(selectedChecker));
                     }
-                    beatCheckers = new List<Checker>();
-                    beatCheckers.AddRange(BeatMoves(selectedChecker));
                     if (beatCheckers.Count == 0)
                     {
                         selectedChecker = null;
                         isWhiteTurn = !isWhiteTurn;
                         beatCheckers = new List<Checker>();
                         beatCheckers.AddRange(BeatMoves());
+                        WinAndDrawCheck();
                     }
                 }
             }
@@ -88,23 +90,20 @@ public class Board : MonoBehaviour
             for (int x = selectedChecker.Data.position.x + deltaX; x != mouseDownPosition.x + deltaX; x += deltaX)
             {
                 if (allCheckers.Where(checker => checker.Data.position.x == x && checker.Data.position.y == z).FirstOrDefault() != null)
-                {
                     checkersOnLine.Add(allCheckers.Where(checker => checker.Data.position.x == x && checker.Data.position.y == z).First());
-                }
                 z += deltaZ;
             }
-            Checker checker = checkersOnLine
-                        .Where(checker => checker.Data.position == new Vector2Int(mouseDownPosition.x, mouseDownPosition.y))
-                        .FirstOrDefault();
+            Checker checker = checkersOnLine.Where(checker => checker.Data.position == new Vector2Int(mouseDownPosition.x, mouseDownPosition.y))
+                                            .FirstOrDefault();
             if (checker != null)
-            {
                 return false;
-            }
             if (!selectedChecker.Data.isKing)
             {
                 int colourMult = selectedChecker.Data.isWhite ? 1 : -1;
                 if (vector2Int.y == colourMult && beatCheckers.Count == 0)
                 {
+                    onlyKingTurns = 0;
+                    wasBeat = false;
                     return true;
                 }
                 if (Mathf.Abs(vector2Int.x) == 2)
@@ -117,11 +116,11 @@ public class Board : MonoBehaviour
                     if (enemy != null)
                     {
                         if (enemy.Data.isKing)
-                        {
                             selectedChecker.MakeKing();
-                        }
                         allCheckers.Remove(enemy);
+                        wasBeat = true;
                         Destroy(enemy.gameObject);
+                        onlyKingTurns = 0;
                         return true;
                     }
                 }
@@ -133,10 +132,14 @@ public class Board : MonoBehaviour
                     Checker[] enemies = checkersOnLine.Where(checker => checker.Data.isWhite != selectedChecker.Data.isWhite).ToArray();
                     if (enemies.Length == 0 && beatCheckers.Count == 0)
                     {
+                        onlyKingTurns++;
+                        wasBeat = false;
                         return true;
                     }
                     if (enemies.Length == 1)
                     {
+                        onlyKingTurns = 0;
+                        wasBeat = true;
                         allCheckers.Remove(enemies[0]);
                         Destroy(enemies[0].gameObject);
                         return true;
@@ -146,49 +149,7 @@ public class Board : MonoBehaviour
         }
         return false;
     }
-    private void AddChecker(CheckerData data)
-    {
-        GameObject checkerPrefab = data.isWhite ? WhitePrefab : BlackPrefab;
-        var figureGameObject = Instantiate(checkerPrefab, new Vector3(data.position.x, 0, data.position.y), checkerPrefab.transform.rotation);
-        if (data.isKing)
-        {
-            figureGameObject.GetComponent<Checker>().MakeKing();
-        }
-        figureGameObject.GetComponent<Checker>().Data = data;
-        allCheckers.Add(figureGameObject.GetComponent<Checker>());
-    }
-    public void SaveBoard()
-    {
-        BoardState boardState;
-        boardState.checkersData = allCheckers.Select(checker => checker.Data).ToArray();
-        boardState.isWhiteTurn = isWhiteTurn;
-        string path = Path.Combine(Application.dataPath, "previousGame.json");
-        using (StreamWriter streamWriter = new StreamWriter(path))
-        {
-            string json = JsonUtility.ToJson(boardState);
-            streamWriter.Write(json);
-        }
-    }
-    public void GenerateBoard(bool isNewGame = false)
-    {
-        foreach (var checker in allCheckers.Where(checker => checker != null))
-        {
-            Destroy(checker.gameObject);
-        }
-        allCheckers = new List<Checker>();
-        string path = isNewGame ? "newGame.json" : "previousGame.json";
-        string fullPath = Path.Combine(Application.dataPath, path);
-        using (StreamReader reader = new StreamReader(fullPath))
-        {
-            string json = reader.ReadToEnd();
-            BoardState boardState = JsonUtility.FromJson<BoardState>(json);
-            isWhiteTurn = boardState.isWhiteTurn;
-            for (int i = 0; i < boardState.checkersData.Length; i++)
-                AddChecker(boardState.checkersData[i]);
-        }
-        beatCheckers = BeatMoves();
-    }
-    private List<Checker> BeatMoves(Checker checkerB = null)
+    private List<Checker> BeatMoves(Checker checkerForOneMoreTurn = null)
     {
         List<Checker> beatCheckers = new List<Checker>();
         List<Checker> checkers;
@@ -199,12 +160,10 @@ public class Board : MonoBehaviour
             new Vector2Int(-1,1)
         };
         Vector2 max = new Vector2(7, 7);
-        if (checkerB == null)
-        {
+        if (checkerForOneMoreTurn == null)
             checkers = allCheckers;
-        }
         else
-            checkers = new List<Checker>() { checkerB };
+            checkers = new List<Checker>() { checkerForOneMoreTurn };
         foreach (Checker checker in allCheckers.Where(checker => checker.Data.isWhite == isWhiteTurn))
         {
             if (checker.Data.isKing)
@@ -215,7 +174,6 @@ public class Board : MonoBehaviour
                     if (!isFound)
                     {
                         int counter = 1;
-                        //while (0 <= (checker.Data.position + step * (counter + 1)).magnitude && (checker.Data.position + step * (counter + 1)).magnitude <= max.magnitude)
                         while (((checker.Data.position + step * (counter + 1)).x >= 0 && (checker.Data.position + step * (counter + 1)).x <= 7)
                             && ((checker.Data.position + step * (counter + 1)).y >= 0 && (checker.Data.position + step * (counter + 1)).y <= 7))
                         {
@@ -239,7 +197,6 @@ public class Board : MonoBehaviour
             {
                 foreach (Vector2Int step in steps)
                 {
-                    //if (0 <= (checker.Data.position + step * 2).magnitude && (checker.Data.position + step * 2).magnitude <= max.magnitude)
                     if (((checker.Data.position + step * 2).x >= 0 && (checker.Data.position + step * 2).x <= 7)
                             && ((checker.Data.position + step * 2).y >= 0 && (checker.Data.position + step * 2).y <= 7))
                     {
@@ -255,16 +212,66 @@ public class Board : MonoBehaviour
         }
         return beatCheckers;
     }
+    public void SaveBoard()
+    {
+        BoardState boardState;
+        boardState.checkersData = allCheckers.Select(checker => checker.Data).ToArray();
+        boardState.isWhiteTurn = isWhiteTurn;
+        boardState.onlyKingTurns = onlyKingTurns;
+        string path = Path.Combine(Application.dataPath, "previousGame.json");
+        using (StreamWriter streamWriter = new StreamWriter(path))
+        {
+            string json = JsonUtility.ToJson(boardState);
+            streamWriter.Write(json);
+        }
+    }
+    public void GenerateBoard(bool isNewGame = false)
+    {
+        foreach (var checker in allCheckers.Where(checker => checker != null))
+            Destroy(checker.gameObject);
+        allCheckers = new List<Checker>();
+        string path = isNewGame ? "newGame.json" : "previousGame.json";
+        string fullPath = Path.Combine(Application.dataPath, path);
+        using (StreamReader reader = new StreamReader(fullPath))
+        {
+            string json = reader.ReadToEnd();
+            BoardState boardState = JsonUtility.FromJson<BoardState>(json);
+            isWhiteTurn = boardState.isWhiteTurn;
+            onlyKingTurns = boardState.onlyKingTurns;
+            for (int i = 0; i < boardState.checkersData.Length; i++)
+                AddChecker(boardState.checkersData[i]);
+        }
+        beatCheckers = BeatMoves();
+    }
+    private void AddChecker(CheckerData data)
+    {
+        GameObject checkerPrefab = data.isWhite ? WhitePrefab : BlackPrefab;
+        var figureGameObject = Instantiate(checkerPrefab, new Vector3(data.position.x, 0, data.position.y), checkerPrefab.transform.rotation);
+        if (data.isKing)
+            figureGameObject.GetComponent<Checker>().MakeKing();
+        figureGameObject.GetComponent<Checker>().Data = data;
+        allCheckers.Add(figureGameObject.GetComponent<Checker>());
+    }
+    private void WinAndDrawCheck()
+    {
+        if (allCheckers.Where(checker => checker.Data.isWhite).Count() == 0)
+            Debug.Log("Black WINS!!!");
+        if (allCheckers.Where(checker => !checker.Data.isWhite).Count() == 0)
+            Debug.Log("White WINS!!!");
+        if (onlyKingTurns == 15)
+            Debug.Log("DRAW");
+    }
 }
-
 [Serializable]
 public struct BoardState
 {
     public CheckerData[] checkersData;
     public bool isWhiteTurn;
-    public BoardState(CheckerData[] checkers, bool isWhiteTurn)
+    public int onlyKingTurns;
+    public BoardState(CheckerData[] checkers, bool isWhiteTurn, int onlyKingTurns)
     {
         this.checkersData = checkers;
         this.isWhiteTurn = isWhiteTurn;
+        this.onlyKingTurns = onlyKingTurns;
     }
 }
--- RussianCheckers/Assets/Scripts/CamerMover.cs
+++ RussianCheckers/Assets/Scripts/CamerMover.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CamerMover : MonoBehaviour
 {
+    
     private void Start()
     {
         transform.position = new Vector3(3.5f, 0, 3.5f);
+
     }
     void Update()
     {
         transform.eulerAngles += new Vector3(Input.GetAxis("Vertical"), -Input.GetAxis("Horizontal"), 0);
+        if (Input.GetMouseButton(1))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.eulerAngles += new Vector3(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0.0f) * Time.deltaTime * 100f;
+            }
+
+            else if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.eulerAngles += new Vector3(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0.0f) * Time.deltaTime * 100f;
+            }
+        }
     }
 }
